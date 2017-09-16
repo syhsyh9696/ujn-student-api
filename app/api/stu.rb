5c576f09a82ea7143d0424c5495b3828c5f4aec6
syhsyh9696 <sunyuanhao123456@gmail.com>
2017-09-16 08:56:41 +0000

    Modify some config files

--- app/api/stu.rb
+++ app/api/stu.rb
@@ -3,6 +3,14 @@ module API
     version 'v1', using: :path # Define API::Version
     format :json
 
+    # Define grape-swagger
+    add_swagger_documentation(
+      mount_path: 'swagger_doc',
+      hide_format: true,
+      hide_documentation_path: true
+    )
+
+    
     helpers do
       def find_student stu_num
         stu_num = stu_num.to_s
@@ -43,17 +51,14 @@ module API
         @student = Student.where("stu_name = ?", stu_name).first
         @student.stu_num
       end
+
+      def find_dept_girls dept_num
+        dept_num = '____' + dept_num + '_____'
+        @dept_girls = Student.where("stu_num like ? AND stu_gender", dept_num, '女')
+      end
     end
 
-
     namespace :student do
-      params do
-        requires :number, type: Integer
-        requires :grade, type: Integer
-        requires :name, type: String
-        requires :dept_num, type: String
-        requires :dept_major, type: String
-      end
       get '/' do
         content_type 'text/plain'
         body "It's an API for UJN. "
@@ -84,18 +89,34 @@ module API
         present info, with: API::Entities::Info
       end
 
-      params do
-        requires :dept_num, type: String
-      end
-      get '/dept/:dept_num' do
-        info = find_dept(params[:dept_num][0..1])
-        present info, with: API::Entities::Info
-      end
+      namespace :dept do
+        params do
+          requires :dept_num, type: String
+        end
+        get '/:dept_num' do
+          info = find_dept(params[:dept_num][0..1])
+          present info, with: API::Entities::Info
+        end
 
-      get '/dept/:dept_num/:dept_major' do
-        info = find_major(params[:dept_num][0..1], params[:dept_major][0..1])
-        present info, with: API::Entities::Info
-      end
+        params do
+          requires :dept_num, type: String
+          requires :dept_major, type: String
+        end
+        get '/:dept_num/:dept_major' do
+          info = find_major(params[:dept_num][0..1], params[:dept_major][0..1])
+          present info, with: API::Entities::Info
+        end
+
+        params do
+          requires :dept_num, type: String
+          # requires :dept_major, type: String
+        end
+        get '/girl/:dept_num' do
+          info = find_dept_girls(params[:dept_num][0..1])
+          present info, with: API::Entities::Info
+        end
+
+      end # Namespace :dept end
 
       namespace :pic do
         get '/url/:number' do
@@ -110,18 +131,10 @@ module API
           stu_num = find_num_with_name(params[:name])
           redirect "../num/#{stu_num}"
         end
-      end
 
-      # Define grape-swagger
-      if Rails.env.development?
-        add_swagger_documentation(
-          mount_path: 'swagger_doc',
-          hide_format: true,
-          hide_documentation_path: true
-        )
-      end
+      end # Namespace :pic end
 
-    end
+    end # Namsapce :student end
 
   end
 end
--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
   GrapeSwaggerRails.options.url = "swagger_doc"
   GrapeSwaggerRails.options.app_name = 'My App'
-  GrapeSwaggerRails.options.app_url = '/api/v1/student'
+  GrapeSwaggerRails.options.app_url = '/api/'
 end
